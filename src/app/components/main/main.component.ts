2a63bee16a9fcb99e3fa82825c1dc6fb235a3e00
EgemenCiftci <egemenciftci@gmail.com>
2022-07-22 02:46:15 +0200

    5 files modified

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -10,10 +10,12 @@ import { ContentHostDirective } from '../../directives/content-host.directive';
 export class MainComponent implements OnInit {
   @ViewChild(ContentHostDirective, { static: true })
   contentHost!: ContentHostDirective;
+  backgroundStyle: string;
 
   constructor(private appService: AppService) {}
 
   ngOnInit() {
     this.appService.container = this.contentHost.viewContainerRef;
+    this.backgroundStyle = localStorage.getItem('backgroundStyle') ?? 'blue';
   }
 }
