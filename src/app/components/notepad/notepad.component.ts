c81be801b7dace29ea9afa6fa6c3efa6f8a9a9dd
EgemenCiftci <egemenciftci@gmail.com>
2022-07-28 10:44:06 +0200

    2 files modified

--- src/app/components/notepad/notepad.component.ts
+++ src/app/components/notepad/notepad.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-notepad',
   templateUrl: './notepad.component.html',
-  styleUrls: ['./notepad.component.css']
+  styleUrls: ['./notepad.component.css'],
 })
 export class NotepadComponent implements OnInit {
+  text: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  open(event: any) {
+    const reader = new FileReader();
+    reader.readAsText(event.file);
   }
 
+  async save() {
+    const handle = await (window as any).showSaveFilePicker();
+  }
 }
